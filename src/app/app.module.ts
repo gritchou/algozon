d9ea177020ab992dece25b2a3ce58c37fb3964af
gritchou <gritchou@gmail.com>
2019-05-05 17:45:18 +0200

    feat(search): add instant-search.js && create first search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgAisModule } from 'angular-instantsearch';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    NgAisModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
