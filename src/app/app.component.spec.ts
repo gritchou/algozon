74ac8c4327ae88388d902edabf81633af01d4fca
gritchou <gritchou@gmail.com>
2019-09-21 18:37:55 +0200

    chore(tests): add missing modules imports to test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,49 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { NgAisModule } from 'angular-instantsearch';
+import { AppSearchBoxComponent } from './widgets/search-box';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AppSearchBoxComponent,
       ],
+      imports: [
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatIconModule,
+        NgAisModule,
+      ]
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'algolia'`, () => {
+  it(`should have as title 'Algolia Angular Material'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('algolia');
+    expect(app.title).toEqual('Algolia Angular Material');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  title = 'Algolia Angular Material';
+
   config = {
     apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
     appId: 'latency',
