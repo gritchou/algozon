5d6ff376e066d42cc07aed078f769fafb5ab9f1e
gritchou <gritchou@gmail.com>
2021-01-19 12:39:15 +0100

    chore(config): switch from public dataset to personal dataset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  title = 'Algolia Angular Material';
+	title = 'Algozon';
 
-  config = {
-    apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-    appId: 'latency',
-    indexName: 'instant_search',
-    routing: true,
-  };
+	// Public ecommerce Dataset configured by Algolia's team
+	// config = {
+	//	 apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+	//	 appId: 'latency',
+	//	 indexName: 'instant_search',
+	//	 routing: true,
+	// };
+
+	// Personal Dataset. apiKey is only usable for Search
+	config = {
+		apiKey: 'c93c9c1cd411d4b08f2f95ecc6c150ac',
+		appId: 'S0QWXUZ3HE',
+		indexName: 'prod_ALGOZON',
+		routing: true,
+	};
 }
