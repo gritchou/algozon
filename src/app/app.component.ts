af6d54de50249683b351924f7c1dd4d8c46c7241
gritchou <gritchou@gmail.com>
2021-02-06 16:30:29 +0100

    chore: switch back to public dataset after private expired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,18 +16,19 @@ export class AppComponent {
 	}
 
 	// Public ecommerce Dataset configured by Algolia's team
-	// config = {
-	//	 apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-	//	 appId: 'latency',
-	//	 indexName: 'instant_search',
-	//	 routing: true,
-	// };
-
-	// Personal Dataset. apiKey is only usable for Search
 	config = {
-		apiKey: 'c93c9c1cd411d4b08f2f95ecc6c150ac',
-		appId: 'S0QWXUZ3HE',
-		indexName: 'prod_ALGOZON',
+		apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+		appId: 'latency',
+		indexName: 'instant_search',
 		routing: true,
 	};
+
+	// Personal Dataset. apiKey is only usable for Search
+	// Expired
+	// config = {
+	// 	apiKey: 'c93c9c1cd411d4b08f2f95ecc6c150ac',
+	// 	appId: 'S0QWXUZ3HE',
+	// 	indexName: 'prod_ALGOZON',
+	// 	routing: true,
+	// };
 }
